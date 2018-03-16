830289edc6b30d7768a24073f9e25d062160507d
Rima Jean <rima.jean@gmail.com>
2018-03-16 16:37:24 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,14 +1,15 @@
 class Doctor
-attr_accessor :name
+  attr_accessor :name
+
 
   def initialize(name)
     @name = name
     @appointments = []
   end
 
-  def add_appointment
+  def add_appointment(appointment)
     @appointments << appointment
-    appointment.doctor << self
+    appointment.doctor = self
   end
 
   def appointments
