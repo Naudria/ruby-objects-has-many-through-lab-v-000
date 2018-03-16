9ed65ceaa4214314d828ea48aa10a29182d2c2d1
Rima Jean <rima.jean@gmail.com>
2018-03-16 16:33:58 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -5,4 +5,17 @@ def initialize(name)
   @name = name
   @songs = []
 end
+
+def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}
+  end
+
 end
